7c676a8bd61b704bfa8630721bf896eb0246e40a
Ali Raeini <a.qaseminejad_raeini@hw.ac.uk>
2026-01-30 20:51:46 +0000

    add libtiff as dependency

---
+++ tests/test_tiff.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import image3d as c3d
+
+cwd = Path(__file__).parent
+
+def test_readPng():
+    img = c3d.VxlImgU8()
+    assert img.data().shape == (0,0,0)
+    img = c3d.VxlImgU8(cwd / "piskelapp.png")
+    img.write("piskelapp.tif")
+    assert Path("piskelapp.tif").exists()
+    img2 = c3d.VxlImgU8("piskelapp.tif")
+    assert img2.data().shape == (72,54,1)
+    print(img2)
+    img2.sliceToPng(normalAxis="z", filename="piskelapp2")
+
+if __name__ == "__main__":
+    # test_version()
+    # test_voxlibI()
+    test_readPng()
+ 
